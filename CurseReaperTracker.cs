d9b7c620b0a15537f992c1377cd2532d58a2be40
TheExplosivePanda <43807128+TheExplosivePanda@users.noreply.github.com>
2021-03-26 10:02:56 +0300

    Add files via upload

---
+++ CurseReaperTracker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+using ItemAPI;
+
+namespace ReaperTracker
+{
+    class CurseReaperTracker : MonoBehaviour
+    {
+
+        void Awake()
+        {
+            SpriteBuilder.SpriteFromResource("ReaperTracker/Resources/ReaperIcon", MinimapspriteObj);
+        }
+
+        GameObject MinimapspriteObj = new GameObject("minimap curse sprote");
+        void Start()
+        {
+                       
+        }
+
+        void Update()
+        {           
+            Vector3 worldPosition = this.gameObject.transform.position;
+            Vector3 minimapPosition = Minimap.Instance.transform.position + new Vector3(worldPosition.x * 0.125f, worldPosition.y * 0.125f, 1f);
+            MinimapspriteObj.transform.position = minimapPosition.Quantize(0.0625f); ;                    
+        }
+        void OnDestroy()
+        {
+            Destroy(MinimapspriteObj);
+        }
+    }
+}
